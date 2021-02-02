8a12915c290b9029ab15ab940f8531455e9519a1
Murad-Hasan <muradhasan.7845@gmail.com>
2021-02-02 19:24:50 +0600

    let_const

---
+++ let_const.js
@@ -0,0 +1,21 @@
+//if variable maybe changed then you use let.
+//if variable maybe not changed then you use const.
+const fName = 'elias kanchon';
+console.log(fName)
+
+const numbers = [12, 45]
+console.log("numbers", numbers)
+numbers[1] = 88
+console.log("numbers", numbers)
+numbers.push(12);
+console.log("numbers", numbers)
+
+const hero = {name: 'sakib khan', phone : 34365436}
+
+hero.phone = 45878695;
+hero.id = 5875; //new value added
+console.log( hero)
+console.log( hero)
+console.log(hero)
+
+
